c65dc723c405177beb8a01e858fa2b270f79d702
prakriti5dhang <prakritidhang@gmail.com>
2026-02-07 11:04:56 +1100

    smevg

---
+++ index.js
@@ -0,0 +1,203 @@
+
+/******************************************* Science *****************************************************/
+
+function showsc1(){
+    document.getElementById("title_exp").innerHTML="Three states of matter";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/225505135/embed";
+  iframe.style.width = "70%";
+  iframe.style.height = "70%";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showsc2(){
+    document.getElementById("title_exp").innerHTML="States of matter";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/225503607/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showsc3(){
+    document.getElementById("title_exp").innerHTML="Particles of matter have space between them";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/225503880/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+function showsc4(){
+    document.getElementById("title_exp").innerHTML="Hooke's Law ";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/209291083/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showsc5(){
+    document.getElementById("title_exp").innerHTML="Electrolysis";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/209296260/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showsc6(){
+    document.getElementById("title_exp").innerHTML="Periodic table";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/235218336/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+
+/******************************************* Maths *****************************************************/
+
+function showm1(){
+    document.getElementById("title_exp").innerHTML="Area of Rhombus ";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/208084573/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showm2(){
+    document.getElementById("title_exp").innerHTML="Polygons ";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/208814214/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showm3(){
+    document.getElementById("title_exp").innerHTML="Medians of triangle are concurrent";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/212048883/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+
+function showm4(){
+    document.getElementById("title_exp").innerHTML="Two right angle triangle are congruent";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/212050433/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showm5(){
+    document.getElementById("title_exp").innerHTML="Pascals Triangle";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/214013834/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+
+/******************************************* Environmental Science *****************************************************/
+
+function showphotosysthesis(){
+    document.getElementById("title_exp").innerHTML="Photosynthesis";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/197860602/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+function showphwatercycle(){
+    document.getElementById("title_exp").innerHTML="Water Cycle";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/208084229/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+
+/******************************************* Games *****************************************************/
+
+function showg1(){
+    document.getElementById("title_exp").innerHTML="Quiz";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/195984588/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showg2(){
+    document.getElementById("title_exp").innerHTML="Apple Catching";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/195427935/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
+function showg3(){
+    document.getElementById("title_exp").innerHTML="Ball and Paddle ";
+    const iframe = document.createElement("iframe");
+  iframe.src = "https://scratch.mit.edu/projects/195347759/embed";
+  iframe.width = "100%";
+  iframe.height = "100";
+  iframe.style.border = "0";
+
+  document.getElementById("iframeexpshow").appendChild(iframe);
+
+}
+
